01c6a4743044c0d0ced03002ea3ba225696e16b6
ideenmolavi <ideenmolavi@gmail.com>
2020-08-26 13:59:04 +0000

    Clog

---
+++ Runtime/CLogData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace LazySamurais.Common
+{
+    [CreateAssetMenu(fileName = "LogData", menuName = "Scriptable Objects/Log Data", order = 1)]
+    [Serializable]
+    public class CLogData : ScriptableObject
+    {
+        [Tooltip("Add filters for unity developer console here.")]
+        [Header("[Log Key]---[Show Log?]")]
+        public StringBoolDictionary Filters = new StringBoolDictionary();
+    }
+
+    [Serializable]
+    public class StringBoolDictionary : SerializableDictionary<string, bool> { }
+}
